d4c032191ea199280dc70f6e69d7c49108a800e7
AnushGurung11 <anushgrg15@gmail.com>
2026-08-02 01:09:44 +0545

    feat: Models for User, post and comment created

---
+++ model/commentModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+    {
+        comment_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            auto: true
+        },
+        post_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "post"
+        },
+        cmt_user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user"
+        },
+        description: {
+            type: String,
+            minlength: 3,
+            maxlength: 150,
+            required: true
+        },
+        cmt_at: {
+            type: Date,
+            default: Date.now
+        },
+        delete_status: {
+            type: Boolean,
+            default: false
+        }
+    }, { timestamps: true }
+)
+
+export default mongoose.model("comment", commentSchema); 
---
+++ model/userModel.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            auto: true
+        },
+        username: {
+            type: String,
+            minlength: 5,
+            maxlength: 50,
+            required: true
+        },
+        email: {
+            type: String,
+            match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            required: true
+        },
+        phone: {
+            type: Number,
+            optional: true
+        },
+        DOB: {
+            type: Date,
+            required: true
+        },
+        role: {
+            type: String,
+            enum: ["user", "admin", "moderator"],
+            default: "user"
+        },
+        created_at: {
+            type: Date,
+            default: Date.now
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        last_login: {
+            type: Date,
+            default: Date.now
+        },
+        active_status: {
+            type: String,
+            enum: ["active", "inactive", "banned", "deleted"],
+            default: "active"
+        }
+    }, { timestamps: true }
+)
+
+// This async fuction is running before saving the credentials to the database
+// The password is hashed and then replaced with the initial password
+userSchema.pre("save", async function () {
+    const saltRound = 10;
+    const hashed_password = await bcrypt.hash(this.password, saltRound)
+    this.password = hashed_password;
+})
+
+// This is a method for this class where it will checked the provied password and compare to the exisiting password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+export default mongoose.model("user", userSchema); 
