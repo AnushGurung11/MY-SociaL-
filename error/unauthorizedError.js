0fd81bda93ef3dc5a2b2d9958b9675b4e6f1184f
AnushGurung11 <anushgrg15@gmail.com>
2026-08-13 23:41:55 +0545

    feat: added error middleware and implemented in server

--- error/unauthorizedError.js
+++ error/unauthorizedError.js
@@ -3,5 +3,6 @@ import { AppError } from "./appError";
 export class UnauthorizedError extends AppError {
   constructor(message = "You do not have permission") {
     super(message, 403);
+    this.isOperational = false;
   }
 }
