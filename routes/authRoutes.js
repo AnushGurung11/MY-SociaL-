2e82988e8157de37798d9377efa6a892111447ee
AnushGurung11 <anushgrg15@gmail.com>
2026-08-15 00:18:04 +0545

    feat: added auth validation middleware in auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,11 +4,13 @@ import {
   loginUserController,
   logOutController,
 } from "../controller/auth.controller.js";
+import { validate } from "../validation/validate.js";
+import { loginSchema, registerSchema } from "../validation/auth.validation.js";
 
 export const authRouter = express.Router();
 
 console.log("authRoutes.js loaded");
 
-authRouter.post("/register", registerUserController);
-authRouter.post("/login", loginUserController);
+authRouter.post("/register", validate(registerSchema), registerUserController);
+authRouter.post("/login", validate(loginSchema), loginUserController);
 authRouter.post("/logout", logOutController);
